a4a68ff379708cd42649f07a08577de021c1d116
Ash Monsh <wh7r.com@gmail.com>
2025-09-17 20:00:21 +0300

    update tailwind css

--- resources/css/filament/admin/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    content: [
-        './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
-        './vendor/awcodes/filament-curator/resources/**/*.blade.php',
-    ],
-};
